7787137c10b5bdbd8f2c67dae3fe8fa8cafbf752
EddyHamzich <poorgamerno1@gmail.com>
2019-10-20 00:06:54 +0200

    Updates

---
+++ precache-manifest.1fd7570201ca843ba5930615af7c097f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9854bdcff07798e194ffee4f268951c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c2e4e7c62d6d661bf86",
+    "url": "/static/css/main.4eca3149.chunk.css"
+  },
+  {
+    "revision": "5e02063cb6d320d8821b",
+    "url": "/static/js/2.c556e3bd.chunk.js"
+  },
+  {
+    "revision": "0c2e4e7c62d6d661bf86",
+    "url": "/static/js/main.888c1b55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e14e6960e11a56dfeb0a85050acaebce",
+    "url": "/static/media/AMD.e14e6960.svg"
+  },
+  {
+    "revision": "c688ef1332868aad882900620616797c",
+    "url": "/static/media/Amazon.c688ef13.svg"
+  },
+  {
+    "revision": "46335db3bd9730c79447cd4ef10a9438",
+    "url": "/static/media/Apple.46335db3.svg"
+  },
+  {
+    "revision": "ab1a472f1b42e4324825a29ff13a8df6",
+    "url": "/static/media/HelveticaNeueMedium.ab1a472f.woff"
+  },
+  {
+    "revision": "b6f842457780e897aa62fb29f701c91c",
+    "url": "/static/media/Tesla.b6f84245.svg"
+  },
+  {
+    "revision": "0c7ccf03c5824fceaaad4fe5fc3ef0f7",
+    "url": "/static/media/austria.0c7ccf03.svg"
+  },
+  {
+    "revision": "6f98b5eb6bef1ff9276d91cda1ba208e",
+    "url": "/static/media/background.6f98b5eb.svg"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/static/media/clear.7f93fbdb.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/static/media/cloudy.ad789581.svg"
+  },
+  {
+    "revision": "930b583a6d962257e68a40aa8d7c4886",
+    "url": "/static/media/favicon.930b583a.svg"
+  },
+  {
+    "revision": "e8b602700ebdac7dd369834a3275bc46",
+    "url": "/static/media/graphql-icon.e8b60270.png"
+  },
+  {
+    "revision": "ad011885da0ff6dc36a9045cf5843e3f",
+    "url": "/static/media/japan.ad011885.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/js-icon.dd60b11e.svg"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/static/media/nodejs-icon.89ccb090.svg"
+  },
+  {
+    "revision": "6f7aeee8bb8ee61edfd134eef3152e22",
+    "url": "/static/media/npm-icon.6f7aeee8.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/static/media/rainy.c915395f.svg"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/static/media/redux-icon.4912f6d8.png"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/static/media/sass-icon.e0f651ac.svg"
+  },
+  {
+    "revision": "9572c60f47bcdebab186f6fe2a8a3b51",
+    "url": "/static/media/slovenia.9572c60f.svg"
+  },
+  {
+    "revision": "f78cba60ad28e30ea060d424f114670a",
+    "url": "/static/media/snowy.f78cba60.svg"
+  },
+  {
+    "revision": "32de4ca773e6dd9b0f32970e4e64a340",
+    "url": "/static/media/switzerland.32de4ca7.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/static/media/thunder.7142c78b.svg"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/static/media/webpack-icon.c500a380.png"
+  }
+]);
